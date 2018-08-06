396c6cafe3beb5af371c2ae66dca50e0459caf3d
Steve Harmeyer <harmeyersteve@gmail.com>
2018-08-06 10:18:54 -0700

    updates

--- src/JsonRpc.php
+++ src/JsonRpc.php
@@ -2,61 +2,19 @@
 
 namespace Submtd\MoneroPhp;
 
-use Http\Client\Common\Plugin\AuthenticationPlugin;
-use Http\Client\Common\PluginClient;
 use Http\Discovery\HttpClientDiscovery;
 use Http\Discovery\MessageFactoryDiscovery;
-use Http\Client\Common\Plugin\HeaderSetPlugin;
-use Http\Message\Authentication\Wsse;
 
 abstract class JsonRpc
 {
     protected $url;
-    protected $username;
-    protected $password;
 
-    public $plugins = [
-        'Authentication',
-        'Header',
-    ];
-
-    public function __construct($url, $username = null, $password = null)
+    public function __construct($url)
     {
         $this->url = $url;
-        $this->username = $username;
-        $this->password = $password;
     }
 
-    private function getPlugins()
-    {
-        $plugins = [];
-        foreach ($this->plugins as $plugin) {
-            if (!method_exists($this, 'get' . $plugin . 'Plugin')) {
-                continue;
-            }
-            $plugins[] = call_user_func([$this, 'get' . $plugin . 'Plugin']);
-        }
-        return $plugins;
-    }
-
-    private function getAuthenticationPlugin()
-    {
-        return new AuthenticationPlugin(new Wsse($this->username, $this->password));
-    }
-
-    private function getHeaderPlugin()
-    {
-        return new HeaderSetPlugin([
-            'Content-Type' => 'application/json',
-        ]);
-    }
-
-    private function getClient()
-    {
-        return new PluginClient(HttpClientDiscovery::find(), $this->getPlugins());
-    }
-
-    private function getRequest($method, $parameters)
+    public function request($method, array $parameters = [])
     {
         $json = [
             'jsonrpc' => '2.0',
@@ -65,11 +23,9 @@ abstract class JsonRpc
             'params' => $parameters,
         ];
         $messageFactory = MessageFactoryDiscovery::find();
-        return $messageFactory->createRequest('POST', $this->url, [], json_encode($json));
-    }
-
-    public function request($method, array $parameters = [])
-    {
-        return $this->getClient()->sendRequest($this->getRequest($method, $parameters));
+        $request = $messageFactory->createRequest('POST', $this->url, ['Content-Type' => 'application/json'], json_encode($json));
+        $client = HttpClientDiscovery::find();
+        $result = $client->sendRequest($request);
+        return $result->getBody()->getContents();
     }
 }
